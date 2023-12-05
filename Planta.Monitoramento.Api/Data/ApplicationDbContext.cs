ec05ab6c6ac4e2d6621bda5e9685765c359838c1
felipecazotti <felipecazotti@gmail.com>
2023-12-05 13:13:22 -0300

    feat: Cria a model de umidade

--- Planta.Monitoramento.Api/Data/ApplicationDbContext.cs
+++ Planta.Monitoramento.Api/Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Planta.Monitoramento.Api.Models;
 
 namespace Planta.Monitoramento.Api.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Umidade> DadosUmidade { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ Planta.Monitoramento.Api/Models/Umidade.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Planta.Monitoramento.Api.Models
+{
+    public class Umidade
+    {
+        [Required(ErrorMessage = "Id é obrigatório")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "TimeStamp é obrigatório ")]
+        public DateTime TimesStamp { get; set; }
+
+        [Required(ErrorMessage = "UmidadeRelativa é obrigatória")]
+        [Range(0.0, 100.0, ErrorMessage = "Valor de UmidadeRelativa fora do limite de 0% a 100%")]
+        public double UmidadeRelativa { get; set; }
+    }
+}
